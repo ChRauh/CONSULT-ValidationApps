0d9b05bafa0c604d9dd4cdf2dbc535bca0803de1
ChRauh <christian.rauh@gmail.com>
2025-09-10 16:48:06 +0200

    Text measures for coding snippets calculated

--- 0_DrawTextSamplesForCoding.R
+++ 0_DrawTextSamplesForCoding.R
@@ -40,13 +40,17 @@
 # 1. Load full text data and extract text snippets
 # 2. (Re-) Calculate text measures for the snippets
 # 3. Draw the four stratified random samples (avoid duplicates across samples to maximise number of obs)
-# 4. Build and store three samples to be fed into the coding app (unique id, keep measure values,, mark ooverlap obs)
+# 4. Build and store three samples to be fed into the coding app (unique id, keep measure values, mark overlap obs)
 
 
 # Packages used #####
 library(tidyverse)
 library(purrr)
 library(tokenizers)
+library(quanteda)
+library(spacyr)
+library(sophistication) # https://github.com/kbenoit/sophistication
+library(patchwork)
 
 
 # Load and prepare consultation data ####
@@ -58,7 +62,7 @@ fbs <- read_rds("C:/Users/rauh/Dropbox/CR data/CommConsultText/Data/FeedbackText
 # N.B.: 'feedback_ref' is a unique id 
 fbs <- fbs %>%
   filter(!text_full == " ") %>% # Drop observations without full text
-  select(c(feedback_ref, policy_stage_short, text_full, n_sentence, starts_with("ne_"), flesch, legalese)) %>% 
+  select(c(feedback_ref, policy_stage_short, text_full, n_sentence, starts_with("ne_"), flesch, legalese, entropy)) %>% 
   rowwise() %>%
   mutate(entities = sum(c_across(starts_with("ne_")), na.rm = TRUE)) %>% # Sum all named entities in text
   ungroup()
@@ -153,16 +157,188 @@ write_rds(fbs, "./data/snippets.rds")
 # Calculate text based measures for the snippets ####
 
 
+# Quanteda corpus object of the snippet texts
+qcorp <- corpus(fbs$snippet, docvars = data.frame(fbs[, c("feedback_ref")]))
+docids <- docvars(qcorp) %>% 
+  mutate(doc_id = as.character(docid(qcorp)))
+
+
+
+# Flesch Reading Ease Score, based on sophistication package
+
+re <- covars_make(qcorp, readability_measure = "Flesch") 
+re$doc_id <- paste0("text",rownames(re))
+
+readability <- merge(docids[ ,c("doc_id", "feedback_ref")],
+                    re[, c("doc_id", "meanSentenceLength", "Flesch")],
+                    by = "doc_id", all.x = T) %>% 
+  rename(snippet_flesch = Flesch,
+         snippet_meanSentenceLength = meanSentenceLength) %>% 
+  select(-doc_id)
+
+fbs <- fbs %>% 
+  left_join(readability, by = "feedback_ref")
+
+
+
+# Named entity counts
+
+ent <- spacy_extract_entity(qcorp, output = "data.frame", type = "all") # Extraction (takes some time)
+
+ent <- ent %>% # Join in doc_ids
+  left_join(docids,
+            by = "doc_id") %>% 
+  select(c(feedback_ref, ent_type, text))
+
+# Summarize: Count number of entity type by each feedback text
+# N.B.: if the respective text didn't contain any NE it is not in the data
+# respectively missing values in the assembled data set need to be filled with zeros 
+ent2 <- 
+  ent %>% 
+  select(-text) %>% # The entity text
+  group_by(feedback_ref) %>% 
+  count(across(everything())) %>% 
+  pivot_wider(id_cols = "feedback_ref", names_from = "ent_type", values_from = "n", values_fill = 0)
+
+names(ent2)[2:ncol(ent2)] <- paste0("NE_", names(ent2)[2:ncol(ent2)]) %>% tolower() # Clarify names
+
+entities <- ent2 %>% 
+  rowwise() %>% 
+  mutate(snippet_ne_sum = sum(c_across(starts_with("ne_")), na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  select(c(feedback_ref, snippet_ne_sum))
+
+# Add to target data - and ensure that missing values are set to zero (cf. above)
+fbs <- fbs %>% 
+  left_join(entities, by = "feedback_ref") %>% 
+  mutate(across(.cols = starts_with("snippet_ne_"), function(x){ifelse(is.na(x), 0, x)}))
+
+
+# Part-of-speech distributions (sophistication/spacyr) - to get the number of sentences as in main analyses
+# Puts out doc_id itself, order not necessarily correct
+pos <- covars_make_pos(qcorp)
+
+pos2 <- pos %>% # Join in doc_ids
+  left_join(docids,
+            by = "doc_id") %>% 
+  select(-doc_id)
+
+fbs <- fbs %>% 
+  left_join(pos2 %>% 
+              select(c(feedback_ref, n_sentence)) %>% 
+              rename(snippet_n_sentence = n_sentence), 
+            by = "feedback_ref")
+
+fbs$snippet_entities <- fbs$snippet_ne_sum/fbs$snippet_n_sentence # Normalize entity count to text length
+
+
+# Legalese (dictionary)
+
+legalese <- read_rds("C:/Users/rauh/Dropbox/CR data/CommConsultText/Data/EU_Legalese_Dict.rds")
+legalese.r <- paste(legalese$token, collapse = "|")
+fbs$snippet_legalese <- str_count(tolower(fbs$snippet), legalese.r)/fbs$snippet_wc # relative frequency of words from legalese dictionary in text
+hist(fbs$snippet_legalese)
+
+test <- fbs %>% filter(snippet_legalese >= .5)
+# remove ultra short snippets! XXX
 
 
 
 
+# Entropy 
+
+# Quanteda stopword list as RegEx
+stops <- paste(stopwords("english"), collapse = " | ")
+stops <- paste(" ", stops, " ", sep = "")
+
+# Prepare the text bits
+fbs$snippet_entropytext <- tolower(fbs$snippet) # Lower case
+fbs$snippet_entropytext <- gsub(stops, " ", fbs$snippet_entropytext, fixed = F) # remove stopwords 
+
+# Calculate entropy per feedback snippet
+fbs$snippet_entropy <- NA
+progress_bar = txtProgressBar(min=0, max=nrow(fbs), style = 3, char="=")
+for (i in 1:nrow(fbs)) {
+  text <- tokens(fbs$snippet_entropytext[i], remove_numbers = T, remove_punct = T, remove_symbols = T, remove_separators = T) # tokenize EU references with some preprocessing
+  text <- as.character(text) # character vector of each individual token
+  if(length(text) == 0){next}
+  freqs <- table(text)/length(text) # relative token frequency
+  fbs$snippet_entropy[i] <- -sum(freqs * log2(freqs)) # entropy formula following Katz/Bommarito II (2014)
+  setTxtProgressBar(progress_bar, value = i) # Progress indicator
+}
+close(progress_bar)
 
 
 
 
+# Compare distribution of text measures between full text and snippets ####
 
-# Draw a test sample ####
+
+# Helper function for distribution comparision
+plot_density_compare <- function(data, var1, var2) {
+  # capture the variable names
+  var1 <- rlang::ensym(var1)
+  var2 <- rlang::ensym(var2)
+  
+  data_long <- data %>%
+    select(!!var1, !!var2) %>%
+    pivot_longer(cols = everything(),
+                 names_to = "variable",
+                 values_to = "value")
+  
+  ggplot(data_long, aes(x = value, fill = variable, color = variable)) +
+    geom_density(alpha = 0.3) +
+    theme_minimal() +
+    labs(x = "Value", y = "Density",
+         title = paste(rlang::as_name(var1), "vs.", rlang::as_name(var2)))+
+    theme(legend.position = "bottom",
+          plot.title = element_text(hjust = 0.5))
+}
+
+# Compare vars
+p1 <- plot_density_compare(fbs, entropy, snippet_entropy)
+p2 <- plot_density_compare(fbs, flesch, snippet_flesch)
+p3 <- plot_density_compare(fbs, entities, snippet_entities)
+p4 <- plot_density_compare(fbs, legalese, snippet_legalese)
+
+# Plot comparison
+(p1+p2) / (p3+p4) +
+  plot_annotation(
+    title = "Comparing the distribution of 'information quality' measures\nacross full texts and shorter snippets for coding"
+  ) &
+  theme(plot.title = element_text(hjust = 0.5))  # center the title
+
+
+ggsave(
+  filename = "./plots/TextMeasuresFullVsSnippets.png",
+  width = 30, height = 25, units = "cm", dpi = 300
+)
+
+
+# Entropy is an issue (shorter text, less potental for variable vocabulary - probably also an issue for the analyses?)
+# Entities is also somewhat smaller for shorter text, but ample variation ...
+
+
+
+
+# Save the snippet information to disc ####
+
+snips <- fbs %>% select(c(feedback_ref, policy_stage_short, starts_with("snippet")))
+write_rds(snips, "./data/snippets_indicators.rds")
+snips <- read_rds("./data/snippets_indicators.rds")
+
+
+
+# Draw coding samples ####
+
+# Prepare population data
+pop <- snips %>% 
+  select(feedback_ref, snippet, policy_stage_short, snippet_entropy, snippet_flesch, snippet_entities, snippet_legalese) %>% 
+  rename(id = feedback_ref,
+         text = snippet,
+         stage = policy_stage_short)
+names(pop) <- names(pop) %>% str_remove("snippet")
+
 
 set.seed(123)  # for reproducibility
 N <- 300
